3b4a13f4bfdb4104c035010b0341ead559c2f868
Serge Pegoff <spegoff@authentica.eu>
2025-11-07 18:27:33 +0100

    no puma plugin for solidqueue

--- app/controllers/testemail_controller.rb
+++ app/controllers/testemail_controller.rb
@@ -2,9 +2,9 @@ class TestemailController < ApplicationController
 require "resend"
   def index
     params = {
-  "from": "gong1 app <onboarding@resend.dev>",
-  "to": ["spegoff@authentica.eu"],
-  "subject": "hello world",
+  "from": "spegoff@authentica.eu",
+  "to": ["spegoff@gmail.com"],
+  "subject": "hello world 2",
   "html": "<strong>it works!</strong>"
 }
 sent = Resend::Emails.send(params)
